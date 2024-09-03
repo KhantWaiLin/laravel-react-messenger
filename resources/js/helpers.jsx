34b99ea875f448851804414a60676fd80aace5c7
Khant-Wai-Lin <6731503060@lamduan.mfu.ac.th>
2024-09-03 15:24:36 +0700

    adding attachment preview and some function to helpers

--- resources/js/helpers.jsx
+++ resources/js/helpers.jsx
@@ -63,3 +63,50 @@ export const isYesterday = (date) => {
         yesterday.getFullYear() === date.getFullYear()
     );
 };
+
+export const isImage = (attachment) => {
+    const mime = attachment.mime || attachment.type;
+    mime = mime.split("/");
+    return mime[0].toLowerCase() === "image";
+};
+
+export const isVideo = (attachment) => {
+    const mime = attachment.mime || attachment.type;
+    mime = mime.split("/");
+    return mime[0].toLowerCase() === "video";
+};
+
+export const isAudio = (attachment) => {
+    const mime = attachment.mime || attachment.type;
+    mime = mime.split("/");
+    return mime[0].toLowerCase() === "audio";
+};
+
+export const isPDF = (attachment) => {
+    const mime = attachment.mime || attachment.type;
+    return mime === "application/pdf";
+};
+
+export const isPreviewable = (attachment) => {
+    return (
+        isImage(attachment) ||
+        isVideo(attachment) ||
+        isAudio(attachment) ||
+        isPDF(attachment)
+    );
+};
+
+export const formatBytesName = (bytes, decimals = 2) => {
+    if (bytes === 0) return "0 Bytes";
+    const k = 1024;
+    const dm = decimals < 0 ? 0 : decimals;
+    const sizes = ["Bytes", "KB", "MB", "GB"];
+
+    let i = 0;
+    let size = bytes;
+    while (size >= k) {
+        size /= k;
+        i++;
+    }
+    return parseFloat(size.toFixed(dm)) + " " + sizes[i];
+};
